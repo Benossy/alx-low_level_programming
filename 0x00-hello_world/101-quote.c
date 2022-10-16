9c0af8ef64459a5288f1830953aff14672062005
Benossy <osinachi.nwachukwu.benneth@gmail.com>
2022-10-16 16:11:19 -0700

    0x01. C - Variables

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - entry
  * Return: 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is usfule\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write("and that piece of art is usfule - Dora Korpar, 2015-10-19, \n");
+	return (0);
