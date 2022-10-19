968b461e69da831faf92317867ba5bdc8e0660a9
Benossy <osinachi.nwachukwu.benneth@gmail.com>
2022-10-19 03:39:27 -0700

    0x02-functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _ads - compute absolut value to integer
+ * @t: the integer to be compute
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
