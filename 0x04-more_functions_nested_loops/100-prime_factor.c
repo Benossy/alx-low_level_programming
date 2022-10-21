af4b259319b583d78d2a13a1799a6a8370b142a8
Benossy <osinachi.nwachukwu.benneth@gmail.com>
2022-10-21 08:11:09 -0700

    0x04-more

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *prints the largest prime factor of the number
+ */
+void main(void)
+{
+	long long num;
+	int maxPrime, oddPrime;
+
+	num = 612852475143;
+	maxPrime = 0;
+	oddPrime = 3;
+
+	while (num % 2 == 0)
+	{
+		maxPrime  = 2;
+		num /= 2;
+	}
+	while (num != 1)
+	{
+		while (num % oddPrime == 0)
+		{
+			maxPrime = oddPrime;
+			num /= oddPrime;
+		}
+		oddPrime += 2;
+	}
+	printf("%d\n", maxPrime);
+}
