be4a9aa8bbd03a91d3707c8ae8a9028d09befef7
Benossy <osinachi.nwachukwu.benneth@gmail.com>
2022-10-24 12:34:35 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - write string followed by new line
  * @str: string
  * Return: void
  */
-
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	while (*str != '\0')
 	{
-		_putchar(*str);
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
