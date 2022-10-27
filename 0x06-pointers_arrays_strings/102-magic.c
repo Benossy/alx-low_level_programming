be6117be569192ae30374e92b1cb78d31984dbfc
Benossy <osinachi.nwachukwu.benneth@gmail.com>
2022-10-27 04:46:22 -0700

    prints

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - magic
+ */
+int main(void)
+{
+	int n;
+	int a['%'];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) - 98;
+
+	printf("a[z] = %d\n", a[2]);
+
+	return (0);
+}
