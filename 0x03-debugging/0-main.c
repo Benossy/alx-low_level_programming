e8ef945e25473b13c52de0c1346b3e7789d70112
Benossy <osinachi.nwachukwu.benneth@gmail.com>
2022-10-30 07:12:03 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - test function that print integer
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
