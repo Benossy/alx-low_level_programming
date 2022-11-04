e2033f0e3f875f76b5caa57b8f5fe5e31cc44176
Benossy <osinachi.nwachukwu.benneth@gmail.com>
2022-11-04 12:24:04 -0700

    0x0A. C - argc, argv

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,12 +4,10 @@ int strlen_no_wilds(char *str);
 void iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
-/**                                                                                                                                   
- * strlen_no_wilds - Returns the length of a string,                                                                                  
- *                   ignoring wildcard characters.                                                                                    
- * @str: The string to be measured.                                                                                                   
- *                                                                                                                                    
- * Return: The length.
+/**
+ * strlen_no_wilds - Returns the length of a string, ignoring wildcard characters.
+ * @str: The string to be measured.
+ * Return: The length
  */
 int strlen_no_wilds(char *str)
 {
@@ -25,9 +23,8 @@ int strlen_no_wilds(char *str)
 	return (len);
 }
 /**
- * iterate_wild - Iterates through a string located at a wildcard                                                                     
- *                until it points to a non-wildcard character.                                                                        
- * @wildstr: The string to be iterated through.                                                                                       
+ * iterate_wild - Iterates through a string located at a wildcard until it points to a non-wildcard character.
+ * @wildstr: The string to be iterated through.
  */
 void iterate_wild(char **wildstr)
 {
@@ -37,15 +34,12 @@ void iterate_wild(char **wildstr)
 		iterate_wild(wildstr);
 	}
 }
-/**                                                                                                                                   
- * postfix_match - Checks if a string str matches the postfix of                                                                      
- *                 another string potentially containing wildcards.                                                                   
- * @str: The string to be matched.                                                                                                    
- * @postfix: The postfix.                                                                                                             
- *                                                                                                                                    
- * Return: If str and postfix are identical - a pointer to the null byte                                                              
- *                                            located at the end of postfix.                                                          
- *         Otherwise - a pointer to the first unmatched character in postfix.                                                         
+/**
+ * postfix_match - Checks if a string str matches the postfix of another string potentially containing wildcards.
+ * @str: The string to be matched.
+ * @postfix: The postfix.
+ * Return: If str and postfix are identical - a pointer to the null byte located at the end of postfix.
+ * Otherwise - a pointer to the first unmatched character in postfix.
  */
 char *postfix_match(char *str, char *postfix)
 {
@@ -64,13 +58,12 @@ char *postfix_match(char *str, char *postfix)
 
 	return (postfix);
 }
-/**                                                                                                                                   
- * wildcmp - Compares two strings, considering wildcard characters.                                                                   
- * @s1: The first string to be compared.                                                                                              
- * @s2: The second string to be compared - may contain wildcards.                                                                     
- *                                                                                                                                    
- * Return: If the strings can be considered identical - 1.                                                                            
- *         Otherwise - 0.                                                                                                             
+/**
+ * wildcmp - Compares two strings, considering wildcard characters.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared - may contain wildcards.
+ * Return: If the strings can be considered identical - 1.
+ * Otherwise - 0.
  */
 int wildcmp(char *s1, char *s2)
 {
